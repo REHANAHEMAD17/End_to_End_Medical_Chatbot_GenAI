85d84dff44cfd806895248af94da11ca39ba073f
REHAN AHEMAD <123437333+REHANAHEMAD17@users.noreply.github.com>
2025-10-24 02:51:25 +0530

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name= 'Generative AI Project',
+    version='0.0.0',
+    author= 'Rehan Ahemad',
+    author_email='rehanahemad2000@gmail.com',
+    packages= find_packages(),
+    install_requires= []
+
+)
