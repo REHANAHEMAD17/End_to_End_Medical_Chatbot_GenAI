1e011bc0cf8e9db03ee24dd6a9b7f473dfc6011a
REHAN AHEMAD <123437333+REHANAHEMAD17@users.noreply.github.com>
2025-10-24 02:41:48 +0530

    Added app.py

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_templete,jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVecotoreStore
+from langchain_openai import OpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents
+from langchain.core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+
+app= Flask(__name__)
+
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+
+os.environ['PINECONE_API_KEY']= PINECONE_API_KEY
+os.environ['OPENAI_API_KEY']= OPENAI_API_KEY
+
+embeddings= download_hugging_face_embeddings()
+
+
+
+index_name= 'medicalbot'
+
+docsearch= PineconeVectorStore.from_existing_index(
+    index_name= index_name, 
+    embedding= embeddings
+)
+
+retriever= docsearch.as_retriever(search_type= 'similarity', search_kwargs= {'k': 3})
+
+llm= OpenAI(temperatur= 0.5, max_tokens= 500)
+prompt= ChatPromptTemplete.from_messages(
+    [
+        ("system", system_prompt),("human", "{input}"),
+
+    ]
+)
+
+
+question_answer_chain =create_stuff_documents_chain(llm, prompt)
+rag_chain= create_retrieval_chain(retriever, question_answer_chain)
+
+@app.route("/")
+def index():
+    return render_templete('chat.html')
+
+@app.route("/get", methods= ["GET", "POST"])
+def chat():
+    msg=request.form['msg']
+    input=msg
+    print(input)
+    response=rag_chain.invoke({"input": msg})
+    print('Response': , response['answer'])
+    return str(response["answer"])
+
+if __name__== '__main__':
+    app.run(host= '0.0.0.0', port= 8080, debug= True)
